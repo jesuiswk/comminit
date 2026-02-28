5dab38edf9d8265b3cc708534324901c4012f832
Kai Wang <jesuiswk@gmail.com>
2026-02-28 12:38:50 -0600

    feat: add custom SVG favicon and update configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,7 +27,7 @@ export default defineNuxtConfig({
           rel: 'stylesheet', 
           href: 'https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@300;400;600;800&family=Syne:wght@400;600;700;800&display=swap' 
         },
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
       ],
       htmlAttrs: {
         lang: 'en',
